8e761dd97097cc9061d02b48cbc91a0a5d437630
Vishal <lahsivvaday@gmail.com>
2025-07-30 20:57:16 +0530

    starting of the code

---
+++ app/components/navbar/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=''>
+        <div className= 'h-20 bg-teal-500'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-100vh w-100%'>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default page
